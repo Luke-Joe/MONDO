e06a280c14bc4440111fbe492462a398c025343a
Luke <luke.lukejoe@gmail.com>
2022-06-23 22:36:29 -0700

    Projectile is no longer deals damage after collision with another object

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class Projectile : MonoBehaviour {
     public float speed = 20f;
     public Rigidbody2D rb;
+    public int bounces;
+
     // Start is called before the first frame update
     void Start() {
         rb = GetComponent<Rigidbody2D>();
         rb.velocity = transform.up * speed;
+        bounces = 0;
     }
 
     void OnCollisionEnter2D(Collision2D collision) {
-        if (collision.gameObject.CompareTag("Player") && rb.velocity.magnitude > 1.0f) {
+
+        if (collision.gameObject.CompareTag("Player") && rb.velocity.magnitude > 0f && bounces == 0) {
+            bounces++;
             collision.gameObject.GetComponent<Health>().TakeDamage(1);
             collision.gameObject.GetComponentInChildren<DamageController>().Flash();
+        } else {
+            bounces++;
         }
     }
 }
