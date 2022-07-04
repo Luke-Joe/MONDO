85680af496317c27dde4024b008ee206e601ae3b
Luke <luke.lukejoe@gmail.com>
2022-07-03 20:31:03 -0700

    Added ball trajectory during charge

--- Assets/Scripts/ThrowController.cs
+++ Assets/Scripts/ThrowController.cs
@@ -13,26 +13,29 @@ public class ThrowController : MonoBehaviour {
     public Transform firepoint;
     public Joystick joystick;
     public GameObject projectile;
-    // public GameObject pointPrefab;
-    // public GameObject[] points;
-    // public int numPoints;
+
+    public GameObject pointPrefab;
+    public GameObject[] points;
+    public int numPoints;
+
     private Rigidbody2D rb;
     private AttackState state;
 
     private Vector2 direction;
-    private Vector2 force;
-    public Vector2 minPower;
-    public Vector2 maxPower;
+    public float power = 1;
+    private float maxPower = 30;
+    private float chargeSpeed = 10;
+
 
     void Start() {
         rb = GetComponent<Rigidbody2D>();
         state = AttackState.holding;
 
-        // points = new GameObject[numPoints];
+        points = new GameObject[numPoints];
 
-        // for (int i = 0; i < numPoints; i++) {
-        //     points[i] = Instantiate(pointPrefab, transform.position, Quaternion.identity);
-        // }
+        for (int i = 0; i < numPoints; i++) {
+            points[i] = Instantiate(pointPrefab, transform.position, Quaternion.identity);
+        }
     }
 
     // Update is called once per frame
@@ -42,18 +45,32 @@ public class ThrowController : MonoBehaviour {
 
         switch (state) {
             case AttackState.holding:
+                for (int i = 0; i < points.Length; i++) {
+                    points[i].SetActive(false);
+                }
                 if (Mathf.Abs(joystick.Horizontal) >= 0.2f || Mathf.Abs(joystick.Vertical) >= 0.2f) {
                     state = AttackState.charging;
                 }
                 break;
             case AttackState.charging:
                 // Scale speed/dmg
+                if (power < maxPower) {
+                    power += Time.deltaTime * chargeSpeed;
+                }
                 if (joystick.Horizontal == 0 && joystick.Vertical == 0) {
                     state = AttackState.attack;
                 }
+                for (int i = 0; i < points.Length; i++) {
+                    points[i].SetActive(true);
+                    points[i].transform.position = TrajectoryPosition(i * 0.02f);
+                }
                 break;
             case AttackState.attack:
+                for (int i = 0; i < points.Length; i++) {
+                    points[i].SetActive(false);
+                }
                 Shoot();
+                power = 0;
                 break;
         }
     }
@@ -63,11 +80,14 @@ public class ThrowController : MonoBehaviour {
     }
 
     void Shoot() {
-        Instantiate(projectile, firepoint.position, firepoint.rotation);
+        GameObject newBullet = Instantiate(projectile, firepoint.position, firepoint.rotation) as GameObject;
+        newBullet.GetComponent<Projectile>().speed += power;
         state = AttackState.holding;
     }
 
-    // Vector2 TrajectoryPosition(float t) {
-    //     Vector2 currPointPos = transform.position 
-    // }
+    Vector2 TrajectoryPosition(float t) {
+        Vector2 currPointPos = (Vector2)firepoint.transform.position + (direction.normalized * power * t);
+
+        return currPointPos;
+    }
 }
