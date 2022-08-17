a674c16a50c1e175fafd3c593c4e36f53ad2798a
Luke <luke.lukejoe@gmail.com>
2022-08-16 19:49:21 -0700

    Completed basic pause menu

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -1,20 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
     public GameObject pauseMenuUI;
 
-    public void OnPause() {
+    public void OnPauseGame() {
         if (!GameManager.gameIsPaused) {
-            Time.timeScale = 0;
-            GameManager.gameIsPaused = true;
+            Pause(); 
         } else {
-            Time.timeScale = 1;
-            GameManager.gameIsPaused = false;
+            Resume();
             // Ball still spawnable on pause -> Change how game is paused
         }
+    }
+
+    public void LoadMenu() {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void RestartGame() {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Classic");
+    }
+
+    void Pause() {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0;
+        GameManager.gameIsPaused = true;
+    }
+
+    void Resume() {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameManager.gameIsPaused = false;
     }
 }
  
