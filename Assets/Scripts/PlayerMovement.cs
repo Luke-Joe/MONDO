347854864898b94d49ff9f2d9a7b364b9a17c158
Luke <luke.lukejoe@gmail.com>
2022-09-02 09:13:23 -0700

    Initial animation controller setup

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,26 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
     public float speed = 5f;
     public Rigidbody2D rb;
     public Joystick joystick;
 
     public Vector2 direction;
 
+    private Animator animator;
+
     // Start is called before the first frame update
-    void Start() {
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
         direction.x = joystick.Horizontal;
         direction.y = joystick.Vertical;
         direction.Normalize();
+        animator.SetFloat("Speed", Math.Abs(direction.x) + Math.Abs(direction.y));
     }
 
-    void FixedUpdate() {
+    void FixedUpdate()
+    {
         rb.MovePosition(rb.position + direction * speed * Time.deltaTime);
     }
 }
