7a4bf51772d56d5b3fca7099ccf3efbf310a3625
Luke <luke.lukejoe@gmail.com>
2022-09-02 16:08:36 -0700

    Added cooldown timer

--- Assets/Scripts/AbilityHolder.cs
+++ Assets/Scripts/AbilityHolder.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class AbilityHolder : MonoBehaviour {
+public class AbilityHolder : MonoBehaviour
+{
     public Ability ability;
+    public Image abilityImage1;
     public KeyCode key;
     private float cooldownTime;
     private float activeTime;
     private bool isActive = false;
 
-    private enum AbilityState {
+    private enum AbilityState
+    {
         ready,
         active,
         cooldown
@@ -17,36 +21,55 @@ public class AbilityHolder : MonoBehaviour {
 
     AbilityState currState = AbilityState.ready;
 
+    void Start()
+    {
+        abilityImage1.fillAmount = 0;
+    }
     // Update is called once per frame
-    void Update() {
-        switch (currState) {
+    void Update()
+    {
+        switch (currState)
+        {
             case AbilityState.ready:
-                if (isActive) {
+                if (isActive)
+                {
                     ability.Activate(gameObject);
                     currState = AbilityState.active;
                     activeTime = ability.activeTime;
+                    abilityImage1.fillAmount = 0;
                 }
                 break;
             case AbilityState.active:
-                if (activeTime > 0) {
+                if (activeTime > 0)
+                {
                     activeTime -= Time.deltaTime;
-                } else {
+                }
+                else
+                {
                     currState = AbilityState.cooldown;
                     cooldownTime = ability.cooldownTime;
+                    abilityImage1.fillAmount = 1;
                     isActive = false;
                 }
                 break;
             case AbilityState.cooldown:
-                if (cooldownTime > 0) {
+                if (cooldownTime > 0)
+                {
                     cooldownTime -= Time.deltaTime;
-                } else {
+                    abilityImage1.fillAmount -= (Time.deltaTime / ability.cooldownTime);
+
+                }
+                else
+                {
+                    Debug.Log("NO LONGER ON CD");
                     currState = AbilityState.ready;
                 }
                 break;
         }
     }
 
-    public void OnClick() {
+    public void OnClick()
+    {
         isActive = true;
     }
 }
